d68148f1b2b18e36065711b776d10fc09903e492
yohan <johan43311@gmail.com>
2023-11-21 15:01:45 +0900

    one-page css 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,44 @@
+import format from "date-fns/format";
 import { useState } from "react";
-import { FiCheck, FiPlus } from "react-icons/fi";
+import { FiCheck, FiChevronLeft, FiChevronRight, FiPlus } from "react-icons/fi";
 import todoItemsDummy from "./assets/dummy/todoItems";
 
 function App() {
   // ctrl + . , mac: cmd + .
+  const [currentDate, setCurrentDate] = useState(new Date());
   const [todoItems, setTodoItems] = useState(todoItemsDummy);
 
+  // yyyy.MM.dd mm:ss
+
   return (
-    <div style={{ position: "relative", height: "100vh" }}>
+    <div style={{ position: "relative", height: "100vh", overflow: "hidden" }}>
+      <div
+        style={{
+          position: "sticky",
+          top: 0,
+          left: 0,
+          right: 0,
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          padding: "20px 0",
+        }}
+      >
+        <div style={{ padding: 10 }}>
+          <FiChevronLeft size={25} />
+        </div>
+        <div style={{ textAlign: "center" }}>
+          <div style={{ fontSize: 24, fontWeight: "bold" }}>
+            {format(currentDate, "MM월 dd일")}
+          </div>
+          <div style={{ color: "#BCBCBC" }}>
+            {format(currentDate, "yyyy년")}
+          </div>
+        </div>
+        <div style={{ padding: 10 }}>
+          <FiChevronRight size={25} />
+        </div>
+      </div>
       <div
         style={{
           padding: 20,
@@ -16,7 +47,13 @@ function App() {
           overflowY: "auto",
         }}
       >
-        {todoItems.map((item) => {
+        {[
+          ...todoItems,
+          ...todoItems,
+          ...todoItems,
+          ...todoItems,
+          ...todoItems,
+        ].map((item) => {
           return (
             <div key={item.id}>
               {!item.isDone && (
@@ -42,7 +79,10 @@ function App() {
                   }}
                 >
                   <div
-                    style={{ textDecoration: "line-through", color: "#666666" }}
+                    style={{
+                      textDecoration: "line-through",
+                      color: "#666666",
+                    }}
                   >
                     {item.content}
                   </div>
@@ -64,10 +104,8 @@ function App() {
           height: 32,
           borderRadius: 30,
           background: "#CFFF48",
-          // text-align vs flex center
-          // text-align: 인라인 요소, text를 정렬하는 속성
-          // <div style={{ textAlign: 'center' }}> <div>텍스트</div> </div>
-          // <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}> <div>텍스트</div> </div>
+          //text-align: 인라인 요소, text를 정렬하는 속성
+          //flex-center: icon같은 애들이 있을땐 얘가 좋다
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
