fee677e60970c63f44801611d8fdb6ca6b8cd9fd
yohan <johan43311@gmail.com>
2023-11-21 13:46:03 +0900

    2023.11.21 Todo-list

---
+++ src/assets/dummy/todoItems.ts
@@ -0,0 +1,56 @@
+export default [
+  {
+    id: 0,
+    content: "안녕하세요, 이것은 투두리스트입니다.",
+    isDone: true,
+    createdAt: "2023-11-19",
+  },
+  {
+    id: 1,
+    content: "여기를 누르면 수정 삭제가 가능합니다.",
+    isDone: false,
+    createdAt: "2023-11-19",
+  },
+  {
+    id: 2,
+    content: "123123",
+    isDone: false,
+    createdAt: "2023-11-20",
+  },
+  {
+    id: 3,
+    content: "456",
+    isDone: true,
+    createdAt: "2023-11-20",
+  },
+  {
+    id: 4,
+    content: "ddsfsfsdf",
+    isDone: false,
+    createdAt: "2023-11-20",
+  },
+  {
+    id: 5,
+    content: "sdfdagsddsd",
+    isDone: false,
+    createdAt: "2023-11-20",
+  },
+  {
+    id: 6,
+    content: "ddsfsfsdf",
+    isDone: false,
+    createdAt: "2023-11-21",
+  },
+  {
+    id: 7,
+    content: "lllllll",
+    isDone: false,
+    createdAt: "2023-11-21",
+  },
+  {
+    id: 8,
+    content: "dfdfsfadf",
+    isDone: false,
+    createdAt: "2023-11-21",
+  },
+];
