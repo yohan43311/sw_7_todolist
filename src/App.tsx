31bc5e9795d85a52cb5fe4915243b7299b3dce9c
최윤혁 <yohan@choeyunhyeog-ui-MacBookAir.local>
2023-11-22 22:26:53 +0900

    변경사항

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   const [todoItems, setTodoItems] = useState(todoItemsDummy);
 
   // yyyy.MM.dd mm:ss
-
+  // 뭐라도 돼라
   return (
     <div style={{ position: "relative", height: "100vh", overflow: "hidden" }}>
       <div
